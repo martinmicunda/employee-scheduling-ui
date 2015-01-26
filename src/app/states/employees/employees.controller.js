ed5fa64cf5ceab74a208cadf27d9cd6049adbaba
martinmicunda <martinmicunda@hotmail.com>
2015-01-26 00:22:18 +0000

    chore(*): clean comments

--- src/app/states/employees/employees.controller.js
+++ src/app/states/employees/employees.controller.js
@@ -13,8 +13,7 @@ class EmployeesController {
         this.employeesPerPage = 10;
         this.listViewTable = true;
     }
-// https://github.com/xmlking/spa-starter-kit/blob/757586ee8289f8d7fe5f35b779d4c16acaa0e317/app/views/providers/providers.results.html
-    // https://github.com/xmlking/spa-starter-kit/blob/d06d69a8f2ea7696117b4a74c2595f33b19c9aee/app/scripts/drug/controllers/DrugResultsController.js
+
     filterEmployees() {
         this.filteredEmployees = this.filterFilter(this.employees, {firstName: this.filterField});
     }
