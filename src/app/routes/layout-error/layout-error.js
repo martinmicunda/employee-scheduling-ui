a79539febef3dcb9df9a8ba1320eef32c1bb22fd
martinmicunda <martinmicunda@hotmail.com>
2015-08-30 18:55:13 +0100

    feat(uni-test): add layout-error tests

--- src/app/routes/layout-error/layout-error.js
+++ src/app/routes/layout-error/layout-error.js
@@ -11,13 +11,13 @@ import templateUnauthorized from './403.html!text';
 import {RouteConfig} from '../../ng-decorators'; // jshint unused: false
 
 //start-non-standard
-@RouteConfig('404', {
-    url: '/404',
-    template: templateNotFound
-})
 @RouteConfig('403', {
     url: '/403',
     template: templateUnauthorized
+})
+@RouteConfig('404', {
+    url: '/404',
+    template: templateNotFound
 })
 @RouteConfig('500', {
     url: '/500',
---
+++ src/app/routes/layout-error/layout-error.spec.js
@@ -0,0 +1,74 @@
+/**
+ * @author    Martin Micunda {@link http://martinmicunda.com}
+ * @copyright Copyright (c) 2015, Martin Micunda
+ * @license   GPL-3.0
+ */
+'use strict';
+
+import './layout-error.js';
+
+describe('ErrorRoute', () => {
+    let $q, $state, $injector;
+
+    beforeEach(angular.mock.module('ngDecorator'));
+
+    beforeEach(inject((_$q_, _$state_, _$injector_) => {
+        $q = _$q_;
+        $state = _$state_;
+        $injector = _$injector_;
+    }));
+
+    describe('403', () => {
+        let url = `/403`,
+            state = '403',
+            currentState;
+
+        beforeEach(inject((_$state_) => {
+            currentState = _$state_.get(state);
+        }));
+
+        it('should have 403 template defined`', () => {
+            expect(currentState.template).toBeDefined();
+        });
+
+        it(`should respond to '${url}' URL`, () => {
+            expect($state.href(state)).toEqual(url);
+        });
+    });
+
+    describe('404', () => {
+        let url = `/404`,
+            state = '404',
+            currentState;
+
+        beforeEach(inject((_$state_) => {
+            currentState = _$state_.get(state);
+        }));
+
+        it('should have 404 template defined`', () => {
+            expect(currentState.template).toBeDefined();
+        });
+
+        it(`should respond to '${url}' URL`, () => {
+            expect($state.href(state)).toEqual(url);
+        });
+    });
+
+    describe('500', () => {
+        let url = `/500`,
+            state = '500',
+            currentState;
+
+        beforeEach(inject((_$state_) => {
+            currentState = _$state_.get(state);
+        }));
+
+        it('should have 500 template defined`', () => {
+            expect(currentState.template).toBeDefined();
+        });
+
+        it(`should respond to '${url}' URL`, () => {
+            expect($state.href(state)).toEqual(url);
+        });
+    });
+});
