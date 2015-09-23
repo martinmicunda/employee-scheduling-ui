b91fb4c8f1ed47db8b139c658abbadcd8620e3e0
martinmicunda <martinmicunda@hotmail.com>
2015-09-23 21:18:19 +0100

    test(routes): add layout-error unit tests

--- src/app/routes/layout-error/layout-error.spec.js
+++ src/app/routes/layout-error/layout-error.spec.js
@@ -5,15 +5,21 @@
  */
 'use strict';
 
+import templateError from './500.html!text';
+import templateNotFound from './404.html!text';
+import templateUnauthorized from './403.html!text';
 import './layout-error.js';
 
 describe('ErrorRoute', () => {
-    let $state;
+    let $state, $compile, $rootScope, scope, render, element;
 
     beforeEach(angular.mock.module('ngDecorator'));
 
-    beforeEach(inject((_$state_) => {
+    beforeEach(inject((_$state_, _$compile_, _$rootScope_) => {
         $state = _$state_;
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        scope = $rootScope.$new();
     }));
 
     describe('403', () => {
@@ -23,6 +29,14 @@ describe('ErrorRoute', () => {
 
         beforeEach(inject((_$state_) => {
             currentState = _$state_.get(state);
+
+            render = () => {
+                let element = angular.element(templateUnauthorized);
+                let compiledElement = $compile(element)(scope);
+                $rootScope.$digest();
+
+                return compiledElement;
+            };
         }));
 
         it('should have 403 template defined`', () => {
@@ -31,6 +45,41 @@ describe('ErrorRoute', () => {
 
         it(`should respond to '${url}' URL`, () => {
             expect($state.href(state)).toEqual(url);
+        });
+
+        it('should have `403` title defined', () => {
+            element = render();
+            const title = angular.element(element[0].querySelector('div.error-code'));
+
+            expect(title.text().trim()).toEqual('403');
+        });
+
+        it('should have `403` message defined', () => {
+            element = render();
+            const message = angular.element(element[0].querySelector('div.error-message'));
+
+            expect(message.text()).toEqual('Unauthorized...');
+        });
+
+        it('should have `403` description defined', () => {
+            element = render();
+            const desc = angular.element(element[0].querySelector('div.error-desc'));
+
+            expect(desc.text().trim()).toEqual('Access forbidden!');
+        });
+
+        it('should have `403` button label defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.text()).toEqual('Go Back to Home Page');
+        });
+
+        it('should have `root` home link defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.attr('href')).toEqual('/');
         });
     });
 
@@ -41,6 +90,14 @@ describe('ErrorRoute', () => {
 
         beforeEach(inject((_$state_) => {
             currentState = _$state_.get(state);
+
+            render = () => {
+                let element = angular.element(templateNotFound);
+                let compiledElement = $compile(element)(scope);
+                $rootScope.$digest();
+
+                return compiledElement;
+            };
         }));
 
         it('should have 404 template defined`', () => {
@@ -49,6 +106,42 @@ describe('ErrorRoute', () => {
 
         it(`should respond to '${url}' URL`, () => {
             expect($state.href(state)).toEqual(url);
+        });
+
+        it('should have `404` title defined', () => {
+            element = render();
+            const title = angular.element(element[0].querySelector('div.error-code'));
+
+            expect(title.text().trim()).toEqual('404');
+        });
+
+        it('should have `404` message defined', () => {
+            element = render();
+            const message = angular.element(element[0].querySelector('div.error-message'));
+
+            expect(message.text()).toEqual(`We couldn't find it...`);
+        });
+
+        it('should have `404` description defined', () => {
+            element = render();
+            const desc = angular.element(element[0].querySelector('div.error-desc'));
+
+            expect(desc.text().trim().includes(`The page you're looking for doesn't exist.`)).toEqual(true);
+            expect(desc.text().trim().includes(`Perhaps, there pages will help find what you're looking for.`)).toEqual(true);
+        });
+
+        it('should have `404` button label defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.text()).toEqual('Go Back to Home Page');
+        });
+
+        it('should have `root` home link defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.attr('href')).toEqual('/');
         });
     });
 
@@ -59,6 +152,14 @@ describe('ErrorRoute', () => {
 
         beforeEach(inject((_$state_) => {
             currentState = _$state_.get(state);
+
+            render = () => {
+                let element = angular.element(templateError);
+                let compiledElement = $compile(element)(scope);
+                $rootScope.$digest();
+
+                return compiledElement;
+            };
         }));
 
         it('should have 500 template defined`', () => {
@@ -67,6 +168,42 @@ describe('ErrorRoute', () => {
 
         it(`should respond to '${url}' URL`, () => {
             expect($state.href(state)).toEqual(url);
+        });
+
+        it('should have `500` title defined', () => {
+            element = render();
+            const title = angular.element(element[0].querySelector('div.error-code'));
+
+            expect(title.text().trim()).toEqual('500');
+        });
+
+        it('should have `500` message defined', () => {
+            element = render();
+            const message = angular.element(element[0].querySelector('div.error-message'));
+
+            expect(message.text()).toEqual(`Oops...`);
+        });
+
+        it('should have `500` description defined', () => {
+            element = render();
+            const desc = angular.element(element[0].querySelector('div.error-desc'));
+
+            expect(desc.text().trim().includes(`Something went terribly wrong.`)).toEqual(true);
+            expect(desc.text().trim().includes(`We are fixing it. Please try again later.`)).toEqual(true);
+        });
+
+        it('should have `500` button label defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.text()).toEqual('Go Back to Home Page');
+        });
+
+        it('should have `root` home link defined', () => {
+            element = render();
+            const label = angular.element(element[0].querySelector('a.btn-success'));
+
+            expect(label.attr('href')).toEqual('/');
         });
     });
 });
