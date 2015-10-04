1d9bfd6bb221c13739b369e20557db882033dcc3
martinmicunda <martinmicunda@hotmail.com>
2015-10-04 01:12:31 +0100

    chore(*): fix the CI build

--- test/test.env.js
+++ test/test.env.js
@@ -1,6 +1,6 @@
 'use strict';
 
 window.ENV =  {
-    mock: false, optimize: false, environment: 'dev'
+    mock: false, optimize: false, environment: 'test'
 };
 System.set('ENV', System.newModule({ 'default': window.ENV, __useDefault: true }));
